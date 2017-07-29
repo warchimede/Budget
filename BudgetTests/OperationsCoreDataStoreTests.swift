1d23c2b32d357f6f42b5329963db4cc216f92528
William Archimede <william.archimede@hoodbrains.com>
2017-07-29 16:42:50 +0200

    [clean-swift]: Updated tests for OperationsCoreDataStore

--- BudgetTests/OperationsCoreDataStoreTests.swift
+++ BudgetTests/OperationsCoreDataStoreTests.swift
@@ -1,8 +1,8 @@
 //
-//  DataPersistenceControllerTests.swift
+//  OperationsCoreDataStoreTests.swift
 //  Budget
 //
-//  Created by William Archimède on 14/07/2017.
+//  Created by William Archimède on 29/07/2017.
 //  Copyright © 2017 William Archimede. All rights reserved.
 //
 
@@ -10,32 +10,32 @@ import XCTest
 import CoreData
 @testable import Budget
 
-class DataPersistenceControllerTests: XCTestCase {
-
+class OperationsCoreDataStoreTests: XCTestCase {
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-
+    
     func testRetrieveOperationsShouldReturn_EmptyArray_When_EmptyBase() {
         // given
         let container = NSPersistentContainer(name: "DataPersistenceControllerTests")
         let persistentStoreDescription = NSPersistentStoreDescription()
         persistentStoreDescription.type = NSInMemoryStoreType
         container.persistentStoreDescriptions = [persistentStoreDescription]
-        let sut = DataPersistenceController(persistentContainer: container)
+        let sut = OperationsCoreDataStore(persistentContainer: container)
 
         // when
-        let operations = try! sut.retrieveOperations()
-
+        let (operations, error) = sut.fetchOperations()
         // then
+        XCTAssertNil(error)
         XCTAssertNotNil(operations)
         XCTAssertEqual(operations.count, 0)
     }
-
+    
 }
